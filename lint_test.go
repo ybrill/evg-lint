909fa5072406841848649036cbf88fbc0b3ecf40
Richard Samuels <richard.samuels@mongodb.com>
2018-02-13 13:19:12 -0500

    Remove golint specific code

--- lint_test.go
+++ lint_test.go
@@ -212,46 +212,6 @@ func TestLine(t *testing.T) {
 	}
 }
 
-func TestLintName(t *testing.T) {
-	tests := []struct {
-		name, want string
-	}{
-		{"foo_bar", "fooBar"},
-		{"foo_bar_baz", "fooBarBaz"},
-		{"Foo_bar", "FooBar"},
-		{"foo_WiFi", "fooWiFi"},
-		{"id", "id"},
-		{"Id", "ID"},
-		{"foo_id", "fooID"},
-		{"fooId", "fooID"},
-		{"fooUid", "fooUID"},
-		{"idFoo", "idFoo"},
-		{"uidFoo", "uidFoo"},
-		{"midIdDle", "midIDDle"},
-		{"APIProxy", "APIProxy"},
-		{"ApiProxy", "APIProxy"},
-		{"apiProxy", "apiProxy"},
-		{"_Leading", "_Leading"},
-		{"___Leading", "_Leading"},
-		{"trailing_", "trailing"},
-		{"trailing___", "trailing"},
-		{"a_b", "aB"},
-		{"a__b", "aB"},
-		{"a___b", "aB"},
-		{"Rpc1150", "RPC1150"},
-		{"case3_1", "case3_1"},
-		{"case3__1", "case3_1"},
-		{"IEEE802_16bit", "IEEE802_16bit"},
-		{"IEEE802_16Bit", "IEEE802_16Bit"},
-	}
-	for _, test := range tests {
-		got := lintName(test.name)
-		if got != test.want {
-			t.Errorf("lintName(%q) = %q, want %q", test.name, got, test.want)
-		}
-	}
-}
-
 func TestExportedType(t *testing.T) {
 	tests := []struct {
 		typString string
